a4e33eea4776b687d763d56009b3bee26fcef384
Zoe McGee <zoe.c.mcgee@gmail.com>
2025-10-31 18:47:22 -0500

    second commit

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
-//const {pagi} = require('../helpers/shortCut')
+// const {organizeRoutes} = require('../helpers/pagination')
 
 router.use(express.static('public'))
 
